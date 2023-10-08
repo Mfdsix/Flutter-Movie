efca73115003bb42f1b3ba4924481f7f2b200d88
mfdsix <mfdsix.1nd0@gmail.com>
2023-10-08 21:41:41 +0700

    fix core

--- core/lib/common/data/models/genre_model.dart
+++ core/lib/common/data/models/genre_model.dart
@@ -1,4 +1,4 @@
-import '../../../movie/lib/domain/entities/genre.dart';
+import 'package:core/common/domain/entities/genre.dart';
 import 'package:equatable/equatable.dart';
 
 class GenreModel extends Equatable {
