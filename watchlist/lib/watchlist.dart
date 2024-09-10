4a36e9595cadc9f294a34c160e9f590f4b779d74
mfdsix <mfdsix.1nd0@gmail.com>
2024-09-10 15:44:08 +0700

    fix export watchlist table

--- watchlist/lib/watchlist.dart
+++ watchlist/lib/watchlist.dart
@@ -1,6 +1,6 @@
 library watchlist;
 
-export 'data/models/watchlist_table.dart';
+export 'domain/entities/watchlist_table.dart';
 export 'domain/usecases/get_watchlist.dart';
 export 'domain/usecases/get_watchlist_status.dart';
 export 'domain/usecases/save_watchlist.dart';
